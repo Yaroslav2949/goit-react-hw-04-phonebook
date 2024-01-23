97df3c5e4f66060cfa4de69f41686cd923d5afe7
Yaroslav Dolynskyi <dolynskiy13@gmail.com>
2024-01-23 19:44:39 +0200

    Commit1.2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,29 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { Container } from './Container/Container';
 import { ContactList } from './ContactList/ContactList';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
- componentDidMount(){
-  // console.log('componentDidMount')
-  const contacts=localStorage.getItem('contacts')
-  const parsedContacts =JSON.parse(contacts)
-  if(parsedContacts){
-    this.setState({contacts:parsedContacts})
+export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem('contacts')) ?? [];
+  });
+  const [filter, setFilter] = useState('');
 
-  }
- 
- }
- 
- componentDidUpdate (prevProps,prevState){
-  // console.log(prevProps,prevState)
- if(prevState.contacts !== this.state.contacts){
-  // console.log('componentDidUpdate')
-  localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-  
- }
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
- }
-
-  //  ..прилітає якесь значення
-  addContact = data => {
-    // кажу шо це значення новий контакт newContact
+  const addContact = ({ name, number } ) => {
     const newContact = {
       id: nanoid(),
-      // name: data.name,
-      // number:data.number
-      // але розпилюю, бо так коротше красівіше і грамотніше
-      ...data,
+      name,
+      number,
     };
-    // деструтуризую ...вічно забуваю, а-а-а-а....
-    const { contacts } = this.state;
+
     // далі провіряю на співпадіння по імені,якщо спіпадає то ахтунг! якщо ні - то на рендер його ...
     if (
       contacts.find(
@@ -51,30 +32,22 @@ export class App extends Component {
     ) {
       alert(`${newContact.name} is already in contacts`);
     } else {
-      this.setState(prevState => ({
+      setContacts(prevContacts => 
         //  рендер- додаємо нове значення в стейт, а решту розпилюємо уникаючи мутації стейту
-        contacts: [newContact, ...prevState.contacts],
-      }));
+        [newContact, ...prevContacts],
+    );
     }
   };
 
-  // приймаю якесь contactId через фільтр повертаю лише ті контакти id яких не співпадає з прийнятим
-  deleteContact = contactId => {
-    this.setState(({ contacts }) => {
-      return {
-        contacts: contacts.filter(contact => contact.id !== contactId),
-      };
-    });
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
   };
   //  пишу функцію яка передає в стейт відразу весь об*єкт значення з інпута "Фільтр контактів - Find contacts by Name"
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
   };
-  // фільтрую контакти за співпадінням
-  getVisibleContacts = () => {
-    //деструктуризую filter, contacts зі стейту
 
-    const { filter, contacts } = this.state;
+  const getVisibleContacts = () => {
     // виношу приведення до ловеркейсу  в інпуті Find contacts by Name (значення в стейті) в окрему змінну
     const NormalizedFilter = filter.toLowerCase();
 
@@ -83,25 +56,124 @@ export class App extends Component {
       contact.name.toLowerCase().includes(NormalizedFilter)
     );
   };
+  const visibleContacts = getVisibleContacts();
 
-  render() {
-    // передаю filter зі і GetVisibleContacts() як пропси у форму для фільтру та списку контактів
-    const { filter } = this.state;
-    // для зручності передаю функцію у змінну і далі пропсом у список контактів
-    const visibleContacts = this.getVisibleContacts();
+  return (
+    <Container>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
 
-    return (
-      <Container>
-        <h1>Phonebook</h1>
-        <ContactForm addContact={this.addContact} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      <ContactList contacts={visibleContacts} onDeleteContact={deleteContact} />
+    </Container>
+  );
+};
 
-        <h2>Contacts</h2>
-        <Filter value={filter} onChange={this.changeFilter} />
-        <ContactList
-          contacts={visibleContacts}
-          onDeleteContact={this.deleteContact}
-        />
-      </Container>
-    );
-  }
-}
+
+
+
+
+
+
+
+// export class App extends Component {
+//   state = {
+//     contacts: [],
+//     filter: '',
+//   };
+//  componentDidMount(){
+// console.log('componentDidMount')
+//   const contacts=localStorage.getItem('contacts')
+//   const parsedContacts =JSON.parse(contacts)
+//   if(parsedContacts){
+//     this.setState({contacts:parsedContacts})
+
+//   }
+
+//  }
+
+//  componentDidUpdate (prevProps,prevState){
+// console.log(prevProps,prevState)
+//  if(prevState.contacts !== this.state.contacts){
+// console.log('componentDidUpdate')
+//   localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+
+//  }
+
+//  }
+
+//  ..прилітає якесь значення
+// addContact = data => {
+// кажу шо це значення новий контакт newContact
+// const newContact = {
+//   id: nanoid(),
+// name: data.name,
+// number:data.number
+// але розпилюю, бо так коротше красівіше і грамотніше
+//   ...data,
+// };
+// деструтуризую ...вічно забуваю, а-а-а-а....
+// const { contacts } = this.state;
+// далі провіряю на співпадіння по імені,якщо спіпадає то ахтунг! якщо ні - то на рендер його ...
+// if (
+//   contacts.find(
+//     contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+//   )
+// ) {
+//   alert(`${newContact.name} is already in contacts`);
+// } else {
+//   this.setState(prevState => ({
+//  рендер- додаємо нове значення в стейт, а решту розпилюємо уникаючи мутації стейту
+//       contacts: [newContact, ...prevState.contacts],
+//     }));
+//   }
+// };
+
+// приймаю якесь contactId через фільтр повертаю лише ті контакти id яких не співпадає з прийнятим
+// deleteContact = contactId => {
+//   this.setState(({ contacts }) => {
+//     return {
+//       contacts: contacts.filter(contact => contact.id !== contactId),
+//     };
+//   });
+// };
+//  пишу функцію яка передає в стейт відразу весь об*єкт значення з інпута "Фільтр контактів - Find contacts by Name"
+// changeFilter = e => {
+//   this.setState({ filter: e.currentTarget.value });
+// };
+// фільтрую контакти за співпадінням
+// getVisibleContacts = () => {
+//деструктуризую filter, contacts зі стейту
+
+// const { filter, contacts } = this.state;
+// виношу приведення до ловеркейсу  в інпуті Find contacts by Name (значення в стейті) в окрему змінну
+// const NormalizedFilter = filter.toLowerCase();
+
+// повертаю через фільтр імена які співпадають з введеними в інпуті Find contacts by Name для їх рендеру а не всього стейту
+//   return contacts.filter(contact =>
+//     contact.name.toLowerCase().includes(NormalizedFilter)
+//   );
+// };
+
+// render() {
+// передаю filter зі і GetVisibleContacts() як пропси у форму для фільтру та списку контактів
+// const { filter } = this.state;
+// для зручності передаю функцію у змінну і далі пропсом у список контактів
+// const visibleContacts = this.getVisibleContacts();
+
+//     return (
+//       <Container>
+//         <h1>Phonebook</h1>
+//         <ContactForm addContact={this.addContact} />
+
+//         <h2>Contacts</h2>
+//         <Filter value={filter} onChange={this.changeFilter} />
+//         <ContactList
+//           contacts={visibleContacts}
+//           onDeleteContact={this.deleteContact}
+//         />
+//       </Container>
+//     );
+//   }
+// }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,91 +1,152 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import css from './ContactForm.module.css'; // стилізація
 import PropTypes from 'prop-types'; // типізація
 import { useState } from 'react';
 
-
-export const ContactForm = () => {
+export const ContactForm = ({ addContact }) => {
   const [name, setName] = useState('');
-  const [number, setNamder] = useState('');
+  const [number, setNamber] = useState('');
 
-  resetForm = () => {
-    return(name === '',number === '') 
-   }
-}
-
-
-
-
-
-
-
-export class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
-  // створюю фунцію очищення форми після сабміту
-  resetForm = () => {
-    this.setState({
-      name: '',
-      number: '',
-    });
-  };
-  // привязую інпути та введені значення
-  onChangeInput = e => {
+  const onChangeInput = e => {
     const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
+    name === 'name' ? setName(value) : setNamber(value);   //порівнюю стейт з властивістю name в input щоб визначити який міняти
+    // або через свіч
+    
+    // switch (name) {
+    //   case 'name':
+    //     setName(value);
+    //     break;
+
+    //   case 'number':
+    //     setNamber(value);
+    //     break;
+    //   default:
+    //     return;
+    // }
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-
-    //  кидаю пропсом ф-цію addContact (передаючи в неї стейт) на сабміт форми  ( handleSubmit) щоб додавати введений контакт
-    this.props.addContact(this.state);
-    this.resetForm();
+    addContact({ name, number });
+    resetForm();
   };
-  render() {
-    const { name, number } = this.state;
-    return (
-      <form className={css.formstyle} onSubmit={this.handleSubmit}>
-        <label className={css.label}>
-          Name
-          <br />
-          <input
-            className={css.input}
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.onChangeInput}
-            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces."
-            required
-          />
-        </label>
+
+  const resetForm = () => {
+    setName('');
+    setNamber('');
+  };
+
+  return (
+    <form className={css.formstyle} onSubmit={handleSubmit}>
+      <label className={css.label}>
+        Name
         <br />
-        <label htmlFor="">
-          Number
-          <br />
-          <input
-            className={css.input}
-            type="tel"
-            name="number"
-            value={number}
-            onChange={this.onChangeInput}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </label>
+        <input
+          className={css.input}
+          type="text"
+          name="name"
+          value={name}
+          onChange={onChangeInput}
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces."
+          required
+        />
+      </label>
+      <br />
+      <label htmlFor="">
+        Number
         <br />
-        <button className={css.button} type="submit">
-          Add Contact
-        </button>
-      </form>
-    );
-  }
-}
+        <input
+          className={css.input}
+          type="tel"
+          name="number"
+          value={number}
+          onChange={onChangeInput}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
+      <br />
+      <button className={css.button} type="submit">
+        Add Contact
+      </button>
+    </form>
+  );
+};
+
+
+
+
+
+// export class ContactForm extends Component {
+// state = {
+//   name: '',
+//   number: '',
+// };
+// створюю фунцію очищення форми після сабміту
+// resetForm = () => {
+//   this.setState({
+//     name: '',
+//     number: '',
+//   });
+// };
+// привязую інпути та введені значення
+// onChangeInput = e => {
+//   const { name, value } = e.currentTarget;
+//   this.setState({ [name]: value });
+// };
+
+// handleSubmit = e => {
+//   e.preventDefault();
+
+//  кидаю пропсом ф-цію addContact (передаючи в неї стейт) на сабміт форми  ( handleSubmit) щоб додавати введений контакт
+// this.props.addContact(this.state);
+// this.resetForm();
+// };
+//   render() {
+//     const { name, number } = this.state;
+//     return (
+//       <form className={css.formstyle} onSubmit={this.handleSubmit}>
+//         <label className={css.label}>
+//           Name
+//           <br />
+//           <input
+//             className={css.input}
+//             type="text"
+//             name="name"
+//             value={name}
+//             onChange={this.onChangeInput}
+//             pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//             title="Name may contain only letters, apostrophe, dash and spaces."
+//             required
+//           />
+//         </label>
+//         <br />
+//         <label htmlFor="">
+//           Number
+//           <br />
+//           <input
+//             className={css.input}
+//             type="tel"
+//             name="number"
+//             value={number}
+//             onChange={this.onChangeInput}
+//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//             required
+//           />
+//         </label>
+//         <br />
+//         <button className={css.button} type="submit">
+//           Add Contact
+//         </button>
+//       </form>
+//     );
+//   }
+// }
 // типізація
+
 ContactForm.propTypes = {
   addContact: PropTypes.func.isRequired, // функція
 };
